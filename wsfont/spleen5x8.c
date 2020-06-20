1e38849c7cc7853bb5dc2e396362b5c91241b011
Frederic Cambus <fred@statdns.com>
2020-06-20 12:18:04 +0200

    Add programs for each font size to allow creating raw font files.
    The resulting exports can be loaded into a wscons display device
    using wsfontload(8).

---
+++ wsfont/spleen5x8.c
@@ -0,0 +1,32 @@
+/*
+ * Spleen 1.7.0
+ * Copyright (c) 2018-2020, Frederic Cambus
+ * https://www.cambus.net/
+ *
+ * Created:      2020-06-20
+ * Last Updated: 2020-06-20
+ *
+ * Spleen is released under the BSD 2-Clause license.
+ * See LICENSE file for details.
+ */
+
+#include <stdio.h>
+#include <time.h>
+
+#include <dev/wscons/wsconsio.h>
+#include <dev/wsfont/spleen5x8.h>
+
+int main() {
+	size_t loop;
+
+	for (loop = 0; loop < 32 * 8; loop++)
+		printf("%c", 0);
+
+	for (loop = 0; loop < 96 * 8; loop++)
+		printf("%c", spleen5x8_data[loop]);
+
+	for (loop = 0; loop < 128 * 8; loop++)
+		printf("%c", 0);
+
+	return 0;
+}
