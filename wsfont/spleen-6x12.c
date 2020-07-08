e209b9f394bbd75a86b457929d4777189bcf3680
Frederic Cambus <fred@statdns.com>
2020-07-08 12:34:42 +0200

    Add a program to allow creating a raw font file for the 6x12 version.

---
+++ wsfont/spleen-6x12.c
@@ -0,0 +1,34 @@
+/*
+ * Spleen 1.8.0
+ * Copyright (c) 2018-2020, Frederic Cambus
+ * https://www.cambus.net/
+ *
+ * Created:      2020-07-08
+ * Last Updated: 2020-07-08
+ *
+ * Spleen is released under the BSD 2-Clause license.
+ * See LICENSE file for details.
+ */
+
+#include <stdio.h>
+#include <time.h>
+
+#include <dev/wscons/wsconsio.h>
+#include <dev/wsfont/spleen6x12.h>
+
+int
+main(int argc, char *argv[])
+{
+	size_t loop;
+
+	for (loop = 0; loop < 32 * 12; loop++)
+		printf("%c", 0);
+
+	for (loop = 0; loop < 96 * 12; loop++)
+		printf("%c", spleen6x12_data[loop]);
+
+	for (loop = 0; loop < 128 * 12; loop++)
+		printf("%c", 0);
+
+	return 0;
+}
